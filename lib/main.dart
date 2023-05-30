d58daa8d28b30d0816da5d18b3813c74e7ca859f
555yash555 <yashpaldps@gmail.com>
2023-05-30 17:46:39 +0530

    added firebase config

--- lib/main.dart
+++ lib/main.dart
@@ -4,34 +4,30 @@ import 'package:firebase_core/firebase_core.dart';
 import 'package:firebase_auth/firebase_auth.dart';
 import 'package:calendar_backend/screens/authform.dart';
 import 'package:calendar_backend/screens/authscreen.dart';
+import 'firebase_options.dart';
 
 void main() async {
   WidgetsFlutterBinding.ensureInitialized();
   await Firebase.initializeApp();
-  runApp( MyApp());
+  runApp(MyApp());
 }
 
 class MyApp extends StatelessWidget {
-
-
   // This widget is the root of your application.
   @override
   Widget build(BuildContext context) {
     return MaterialApp(
-      home:StreamBuilder(builder: (context,usersnapshot){
-      if(usersnapshot.hasData){
-        return Home();
-      }
-      else{
-        return AuthScreen();
-      }
-    },
-        stream: FirebaseAuth.instance.authStateChanges(),) ,
+      home: StreamBuilder(
+        builder: (context, usersnapshot) {
+          if (usersnapshot.hasData) {
+            return Home();
+          } else {
+            return AuthScreen();
+          }
+        },
+        stream: FirebaseAuth.instance.authStateChanges(),
+      ),
       debugShowCheckedModeBanner: false,
-    ) ;
+    );
   }
 }
-
-
-
-
